cd6a80c2e84a9c34bd997407e155f3f13e13f9b5
Renan Moreira <renanj-moreira@outlook.com>
2023-07-26 12:36:03 -0300

    fix: reset checkbox options on refreshing page

--- components/PipelineOptions.vue
+++ components/PipelineOptions.vue
@@ -20,16 +20,17 @@ function changeSelectedOption(e, option) {
 
   <div class="flex justify-center w-full">
     <div class="grid gap-3 grid-cols-4 w-[50%] mb-5 text-[14px]">
-      <div class="relative w-full" v-for="option in options">
+      <div class="relative w-full" v-for="option in options" :key="option">
         <input
+          :id="option"
           type="checkbox"
-          :id="`option-${option.toLowerCase()}`"
           v-bind:onClick="(e) => changeSelectedOption(e, option)"
-          v-bind:checked="selectedOption == option"
+          v-model="selectedOption"
+          :true-value="option"
           class="checked:before:absolute checked:before:-top-[30%] checked:before:-left-[20%] checked:before:scale-75 checked:before:content-[url('/assets/icons/checkmark.svg')] checked:bg-[#2462d1] checked:hover:bg-[#2462d1] checked:border-[#2462d1] peer absolute top-[30%] left-4 cursor-pointer w-[18px] h-[17px] border-[#cbd5e0] border-[2px] border-gray-200 rounded hover:bg-[#edf2f7] appearance-none"
         />
         <label
-          :for="`option-${option.toLowerCase()}`"
+          :for="option"
           class="inline-block w-full cursor-pointer select-none py-3 px-12 border-[1px] border-gray-200 rounded-xl border-[#e2e8f0] hover:bg-[#edf2f7] peer-checked:cursor-default peer-checked:hover:bg-white peer-checked:border-[#2462d1] peer-checked:border-[2px]"
         >
           {{ option }}
